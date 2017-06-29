6f34446b41db93debe73e9791b3e19c97a370972
22427 <zietlow.timon@gmail.com>
2017-06-29 14:03:45 +0200

    added conversion to VD and from VD to the example tool

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <assert.h>
 #include <sstream>
 #include <string>
-#include "vd.h"
+#include "include/vd.h"
 #include <iostream>
 
 #include <glm/glm.hpp>
@@ -24,16 +24,57 @@ int main(int argc, char ** argv)
 		print_help();
 		return 1;
 	}
-	Mesh m;	
-	if(!MeshOPS::load(m, argv[1]))
+	std::string pin = argv[1];
+	std::string ext_in = pin.substr(pin.find_last_of('.')+1);
+	for(char &c : ext_in) c= toupper(c);
+	std::string pout = argv[2];
+	std::string ext_out = pout.substr(pout.find_last_of('.')+1);
+	for(char &c : ext_out) c= toupper(c);
+	Mesh m;
+
+	if(ext_in != "VD")
 	{
-		print_help();
-		return 2;			
+		if(!MeshOPS::load(m, argv[1]))
+		{
+			print_help();
+			return 2;
+		}
 	}
-	if(!MeshOPS::write(m, argv[2]))
+	else
 	{
-		print_help();
-		return 3;
+		VertexData vd;
+		if(!VertexDataOPS::read(vd,pin))
+			return 2;
+		VertexDataOPS::pack_to_mesh(vd,m);
+	}
+	// output
+	if(ext_out != "VD")
+	{
+		if(!MeshOPS::write(m, argv[2]))
+		{
+			print_help();
+			return 3;
+		}
+	}
+
+	else
+	{
+		VertexConfiguration cfg;
+		cfg.set_attribute(AID_POSITION,3,FLOAT,false);
+		cfg.set_attribute(AID_NORMAL,3,FLOAT,false);
+		cfg.set_attribute(AID_TEXCOORD,2,FLOAT,false);
+		cfg.set_attribute(AID_COLOR,4,UNSIGNED_BYTE,true);
+		VertexData vd(PRIM_TRIANGLES,cfg);
+		printf("Note: outputting 'standard' VD with the following attributes:\n\n");
+		printf("\tAttribute | #elem |  Type  | Normalized\n");
+		printf("\t----------+-------+--------+-----------\n");
+		printf("\tPOSITION  |   3   | FLOAT  | false\n");
+		printf("\tNORMAL    |   3   | FLOAT  | false\n");
+		printf("\tTEXCOORD  |   2   | FLOAT  | false\n");
+		printf("\tCOLOR     |   4   | U_BYTE | true\n");
+		VertexDataOPS::pack_from_mesh(vd,&m);
+		if(!VertexDataOPS::write(vd,pout))
+			return 3;
 	}
 
 
